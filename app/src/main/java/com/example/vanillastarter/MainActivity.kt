308db87e9aa459b1b161f358dbe29188b359af12
risal098 <yesitsme811@gmail.com>
2024-12-29 01:07:14 +0700

    dummy

--- app/src/main/java/com/example/vanillastarter/MainActivity.kt
+++ app/src/main/java/com/example/vanillastarter/MainActivity.kt
@@ -3,15 +3,38 @@ package com.example.vanillastarter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.vanillastarter.ui.theme.VanillastarterTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
+/*
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "app-database"
+        ).build()
+        val viewModel: DataViewModel by viewModels { DataViewModelFactory(db.dataDao()) }
+
+        setContent {
+            MaterialTheme {
+                DataApp(viewModel)
+            }
+        }
+    }
+}
+
+*/
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
